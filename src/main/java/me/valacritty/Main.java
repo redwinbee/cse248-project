f5bb3724f80f8e740b7cd79333666491c783a80f
Edwin Bermudes <74934197+valacritty@users.noreply.github.com>
2023-09-06 13:35:34 -0400

    convert to javafx project

--- src/main/java/me/valacritty/Main.java
+++ src/main/java/me/valacritty/Main.java
@@ -1,16 +1,38 @@
 package me.valacritty;
 
+import atlantafx.base.theme.PrimerLight;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import me.valacritty.models.Instructor;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        Scene scene = new Scene(parent, 320, 240);
+        primaryStage.setTitle("Project01");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        deserializeData();
+    }
+
+    private void deserializeData() {
         // register our custom deserializer
         ObjectMapper mapper = new ObjectMapper();
         SimpleModule module = new SimpleModule();
