716c679f7ac1207ce55e827f7532d598e1247b38
Edwin Bermudes <74934197+valacritty@users.noreply.github.com>
2023-09-18 09:41:55 -0400

    avoid duplicate code

--- src/main/java/me/valacritty/utils/Parser.java
+++ src/main/java/me/valacritty/utils/Parser.java
@@ -21,7 +21,77 @@ public class Parser {
     private Parser() {
     }
 
-    private static ArrayList<Day> parseDays(String charStr) {
+    public static Parser getInstance() {
+        if (instance == null) {
+            instance = new Parser();
+        }
+        return instance;
+    }
+
+    private static List<List<String>> splitData(List<String> data) {
+        List<List<String>> instructorsData = new LinkedList<>();
+        List<String> currentInstructorData = new LinkedList<>();
+        instructorsData.add(currentInstructorData);
+
+        for (String str : data) {
+            if (str.equals(DELIMITER)) {
+                currentInstructorData = new LinkedList<>();
+                instructorsData.add(currentInstructorData);
+            } else {
+                currentInstructorData.add(str);
+            }
+        }
+        return instructorsData;
+    }
+
+    public TreeSet<Instructor> parse() {
+        List<List<String>> data = getCsvData();
+        TreeSet<Instructor> instructors = new TreeSet<>();
+        for (List<String> ins : data) {
+            if (ins.isEmpty())
+                continue;
+            instructors.add(createInstructorFromData(ins));
+        }
+
+        return instructors;
+    }
+
+    private List<List<String>> getCsvData() {
+        try (CSVReader reader = new CSVReader(new FileReader(PATH))) {
+            List<String> csvData = reader.readAll().stream().flatMap(Arrays::stream).toList();
+            return splitData(csvData);
+        } catch (IOException | CsvException ex) {
+            System.err.println("An error occurred while reading the CSV data: " + ex.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+    private Instructor createInstructorFromData(List<String> ins) {
+        Instructor out = new Instructor();
+        out.setId(ins.get(0));
+        out.setName(ins.get(1));
+        out.setHomeCampus(parseCampus(ins.get(15)));
+        out.setHomePhone(ins.get(2));
+        out.setWorkPhone(ins.get(30));
+        out.setAddress(ins.get(16) + " ," + ins.get(31));
+        out.setDateHired(parseDateHired(ins.get(17)));
+        out.setCourses(parseCourses(ins.get(32)));
+        out.setRank(ins.get(3));
+        out.setCanTeachOnline(parseCanTeachOnline(ins.get(4)));
+        out.setPreferredCampuses(parsePreferredCampuses(ins.get(5)));
+        out.setCanTeachSecondCourse(getBooleanFromString(ins.get(6)));
+        out.setCanTeachThirdCourse(getBooleanFromString(ins.get(21)));
+        out.setAvailableEarlyMornings(parseAvailableWeekdays(ins.get(8)));
+        out.setAvailableMornings(parseAvailableWeekdays(ins.get(23)));
+        out.setAvailableEarlyAfternoons(parseAvailableWeekdays(ins.get(9)));
+        out.setAvailableAfternoons(parseAvailableWeekdays(ins.get(24)));
+        out.setAvailableEvenings(parseAvailableWeekdays(ins.get(11)));
+        out.setAvailableWeekends(parseAvailableWeekends(ins.get(10) + ins.get(25)));
+
+        return out;
+    }
+
+    private ArrayList<Day> parseDays(String charStr) {
         ArrayList<Day> out = new ArrayList<>(7);
         boolean sawTuesday = false;
         for (String day : charStr.chars().mapToObj(Character::toString).toList()) {
@@ -46,7 +116,7 @@ public class Parser {
         return out;
     }
 
-    private static Campus parseCampus(String campusStr) {
+    private Campus parseCampus(String campusStr) {
         switch (campusStr.charAt(0)) {
             case 'A' -> {
                 return Campus.AMERMAN;
@@ -66,74 +136,6 @@ public class Parser {
         }
     }
 
-    private static List<List<String>> splitData(List<String> data) {
-        List<List<String>> instructorsData = new LinkedList<>();
-        List<String> currentInstructorData = new LinkedList<>();
-        instructorsData.add(currentInstructorData);
-
-        for (String str : data) {
-            if (str.equals(DELIMITER)) {
-                currentInstructorData = new LinkedList<>();
-                instructorsData.add(currentInstructorData);
-            } else {
-                currentInstructorData.add(str);
-            }
-        }
-        return instructorsData;
-    }
-
-    public static Parser getInstance() {
-        if (instance == null) {
-            instance = new Parser();
-        }
-        return instance;
-    }
-
-    public TreeSet<Instructor> parse() {
-        List<List<String>> data = new LinkedList<>();
-        try (CSVReader reader = new CSVReader(new FileReader(PATH))) {
-            List<String> csvData = reader.readAll().stream().flatMap(Arrays::stream).toList();
-            data = splitData(csvData);
-        } catch (IOException | CsvException ex) {
-            System.err.println("An error occurred while reading the CSV data: " + ex.getMessage());
-        }
-
-        // create instructors out of the data and save them to the list
-        TreeSet<Instructor> instructors = new TreeSet<>();
-        for (List<String> ins : data) {
-            if (ins.isEmpty())
-                continue;
-            instructors.add(createInstructorFromData(ins));
-        }
-
-        return instructors;
-    }
-
-    private Instructor createInstructorFromData(List<String> ins) {
-        Instructor out = new Instructor();
-        out.setId(ins.get(0));
-        out.setName(ins.get(1));
-        out.setHomeCampus(parseCampus(ins.get(15)));
-        out.setHomePhone(ins.get(2));
-        out.setWorkPhone(ins.get(30));
-        out.setAddress(ins.get(16) + " ," + ins.get(31));
-        out.setDateHired(parseDateHired(ins.get(17)));
-        out.setCourses(parseCourses(ins.get(32)));
-        out.setRank(ins.get(3));
-        out.setCanTeachOnline(parseCanTeachOnline(ins.get(4)));
-        out.setPreferredCampuses(parsePreferredCampuses(ins.get(5)));
-        out.setCanTeachSecondCourse(getBooleanFromString(ins.get(6)));
-        out.setCanTeachThirdCourse(getBooleanFromString(ins.get(21)));
-        out.setAvailableEarlyMornings(parseAvailableEarlyMornings(ins.get(8)));
-        out.setAvailableMornings(parseAvailableMornings(ins.get(23)));
-        out.setAvailableEarlyAfternoons(parseAvailableEarlyAfternoons(ins.get(9)));
-        out.setAvailableAfternoons(parseAvailableAfternoons(ins.get(24)));
-        out.setAvailableEvenings(parseAvailableEvenings(ins.get(11)));
-        out.setAvailableWeekends(parseAvailableWeekends(ins.get(10) + ins.get(25)));
-
-        return out;
-    }
-
     private ArrayList<Day> parseAvailableWeekends(String weekendsStr) {
         ArrayList<Day> out = new ArrayList<>(2);
         String[] split = weekendsStr.split("(?=(Sun))");
@@ -147,34 +149,14 @@ public class Parser {
         return out;
     }
 
-    private ArrayList<Day> parseAvailableEvenings(String eveningsStr) {
-        String cleaned = removeAsterisks(eveningsStr);
-        return parseDays(cleaned);
-    }
-
-    private ArrayList<Day> parseAvailableAfternoons(String afternoonsStr) {
-        String cleaned = removeAsterisks(afternoonsStr);
-        return parseDays(cleaned);
-    }
-
-    private ArrayList<Day> parseAvailableEarlyAfternoons(String earlyAfternoonsStr) {
-        String cleaned = removeAsterisks(earlyAfternoonsStr);
-        return parseDays(cleaned);
-    }
-
-    private ArrayList<Day> parseAvailableMornings(String morningsStr) {
-        String cleaned = removeAsterisks(morningsStr);
-        return parseDays(cleaned);
-    }
-
-    private ArrayList<Day> parseAvailableEarlyMornings(String earlyMorningsStr) {
-        String cleaned = removeAsterisks(earlyMorningsStr);
+    private ArrayList<Day> parseAvailableWeekdays(String availabilityStr) {
+        String cleaned = removeAsterisks(availabilityStr);
         return parseDays(cleaned);
     }
 
     private HashSet<Campus> parsePreferredCampuses(String parsePreferredCampusesStr) {
         return Arrays.stream(parsePreferredCampusesStr.split("\\s+"))
-                .map(Parser::parseCampus)
+                .map(this::parseCampus)
                 .collect(Collectors.toCollection(HashSet::new));
     }
 
