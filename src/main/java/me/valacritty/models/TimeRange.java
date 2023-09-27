a0ca8e39a497a37aa1db9e055b86e4094fb26554
Edwin Bermudes <74934197+valacritty@users.noreply.github.com>
2023-09-27 16:50:50 -0400

    associate a time range for each time of day

--- src/main/java/me/valacritty/models/TimeRange.java
+++ src/main/java/me/valacritty/models/TimeRange.java
@@ -1,15 +1,36 @@
 package me.valacritty.models;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.util.StringJoiner;
 
 public class TimeRange {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
+    /**
+     * This constructor will use the LocalTime for the time portion of the internal date/time
+     * which would normally fail to be created, but we instead use the UNIX Epoch as the date part
+     * to allow a LocalDateTime to be created. This method should only be used when the time
+     * part is the only relevant part.
+     *
+     * @param startTime The start time
+     * @param endTime   The end time
+     */
+    public TimeRange(LocalTime startTime, LocalTime endTime) {
+        // we don't care about the date when we use this constructor, just use the unix epoch
+        this.startTime = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC).with(startTime);
+        this.endTime = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC).with(endTime);
+    }
+
     public TimeRange(LocalDateTime startTime, LocalDateTime endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public boolean isInRange(LocalTime otherTime) {
+        return otherTime.isAfter(startTime.toLocalTime()) && otherTime.isBefore(endTime.toLocalTime());
     }
 
     public LocalDateTime getStartTime() {
--- src/main/java/me/valacritty/models/enums/TimeOfDay.java
+++ src/main/java/me/valacritty/models/enums/TimeOfDay.java
@@ -1,10 +1,20 @@
 package me.valacritty.models.enums;
 
+import me.valacritty.models.TimeRange;
+
+import java.time.LocalTime;
+
 public enum TimeOfDay {
-    EARLY_MORNING,
-    MORNING,
-    EARLY_AFTERNOON,
-    AFTERNOON,
-    EVENING,
-    IRRELEVANT, // TODO I don't like this enum but it works for now I guess
+    EARLY_MORNING(new TimeRange(LocalTime.of(7, 0), LocalTime.of(8, 0))),
+    MORNING(new TimeRange(LocalTime.of(8, 0), LocalTime.of(12, 0))),
+    EARLY_AFTERNOON(new TimeRange(LocalTime.of(12, 0), LocalTime.of(15, 0))),
+    AFTERNOON(new TimeRange(LocalTime.of(15, 0), LocalTime.of(16, 0))),
+    EVENING(new TimeRange(LocalTime.of(16, 0), LocalTime.of(21, 0))),
+    IRRELEVANT(null); // TODO I don't like this enum but it works for now I guess
+
+    private final TimeRange range;
+
+    TimeOfDay(TimeRange range) {
+        this.range = range;
+    }
 }
